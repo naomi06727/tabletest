f70bd92e74598696403c2e243ae1a4be2c570f79
naomi06727 <80743124+naomi06727@users.noreply.github.com>
2021-03-24 21:42:40 +0800

    TEST
    TEST

--- access.php
+++ access.php
@@ -4,31 +4,9 @@
  * 
  * 
  * */
-// $items = json_decode($_POST['items'],true);
+$items = json_decode($_POST['items'],true);
 
-// echo "<pre>".print_r($items, true)."</pre>";exit;
-// if($items)
-// {
-//     foreach($items as $item)
-//     {
-//         if($item['item_upd']=='add')
-//         {
-//             $sql = "insert into........";
-//         }
-//         elseif($item['item_upd']=='up')
-//         {
-//             $sql = "update ...... set........";
-//         }
-//     }
-// }
-
-
-/**方法2:用ajax傳送
- * 
- * */
-$items = $_POST['items'];
 echo "<pre>".print_r($items, true)."</pre>";exit;
-
 if($items)
 {
     foreach($items as $item)
@@ -44,9 +22,31 @@ if($items)
     }
 }
 
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode(array('status'=>'success','msg' => '上傳/更新完畢'));
-exit;	
+
+/**方法2:用ajax傳送
+ * 
+ * */
+// $items = $_POST['items'];
+// echo "<pre>".print_r($items, true)."</pre>";exit;
+
+// if($items)
+// {
+//     foreach($items as $item)
+//     {
+//         if($item['item_upd']=='add')
+//         {
+//             $sql = "insert into........";
+//         }
+//         elseif($item['item_upd']=='up')
+//         {
+//             $sql = "update ...... set........";
+//         }
+//     }
+// }
+
+// header('Content-Type: application/json; charset=utf-8');
+// echo json_encode(array('status'=>'success','msg' => '上傳/更新完畢'));
+// exit;	
 
 
 ?>
